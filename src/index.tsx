cdc2be6f32107dca0c613e9e605b2b83d8406c53
Ming-der Wang <mingderwang@gmail.com>
2021-09-01 21:29:43 +0800

    get context

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,8 @@
-import ForgeUI, { render, Macro, Button, Text, useEffect, useState, Fragment } from '@forge/ui';
-
-const sendLog = async () => Promise.resolve('send!');
-
-const LogData = ({ counter }) => {
-  const [logSend, setLogSend] = useState(null);
-
-  useEffect(async () => {
-    await sendLog();
-
-    setLogSend(Date.now());
-  }, [counter]);
-
-  return <Text>Last log: {logSend}</Text>;
-};
+import ForgeUI, { useProductContext, Text, Macro, render } from '@forge/ui';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  return (
-    <Fragment>
-      <Button
-        text={`Count is ${count}`}
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      />
-      <LogData counter={count} />
-    </Fragment>
-  );
+  const context = useProductContext();
+  return <Text>All info about my context: {JSON.stringify(context)}</Text>;
 };
 
 export const run = render(<Macro app={<App />} />);
