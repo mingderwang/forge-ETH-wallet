da209bbecf231422196c7e3ed9a584e923eb9a7d
Ming-der Wang <mingderwang@gmail.com>
2021-09-01 23:41:27 +0800

    ok for config page

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,35 @@
-import ForgeUI, { useProductContext, Text, Macro, render } from '@forge/ui';
+import ForgeUI, {
+  useConfig,
+  Text,
+  TextField,
+  Macro,
+  MacroConfig,
+  render,
+} from '@forge/ui';
 
 const App = () => {
-  const context = useProductContext();
-  return <Text>All info about my context: {JSON.stringify(context)}</Text>;
+  // Retrieve the configuration
+  const config = useConfig();
+  console.log(config)
+
+  // Use the configuration values
+  return (
+    <Text>
+      {config.name === undefined?'x':config.name} is {config.age === undefined?'x':config.age} years old.
+    </Text>
+  );
 };
 
 export const run = render(<Macro app={<App />} />);
+
+// Function that defines the configuration UI
+const Config = () => {
+  return (
+    <MacroConfig>
+      <TextField name="name" label="Pet name" />
+      <TextField name="age" label="Pet age" />
+    </MacroConfig>
+  );
+};
+
+export const config = render(<Config />);
