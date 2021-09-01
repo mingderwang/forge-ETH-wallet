e5de28d143193b57f458436e718fbf4e93ae9756
Ming-der Wang <mingderwang@gmail.com>
2021-09-01 21:15:14 +0800

    use setLog

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,32 @@
-import ForgeUI, { useAction, Button, Macro, render } from '@forge/ui'
+import ForgeUI, { render, Macro, Button, Text, useEffect, useState, Fragment } from '@forge/ui';
+
+const sendLog = async () => Promise.resolve('send!');
+
+const LogData = ({ counter }) => {
+  const [logSend, setLogSend] = useState(null);
+
+  useEffect(async () => {
+    await sendLog();
+
+    setLogSend(Date.now());
+  }, [counter]);
+
+  return <Text>Last log: {logSend}</Text>;
+};
 
 const App = () => {
-  const [count, setCount] = useAction(
-    (currentCount, step: number) => currentCount + step,
-    0,
-  )
+  const [count, setCount] = useState(0);
   return (
-    <Button
-      text={`Count is ${count}`}
-      onClick={() => {
-        // what we call "setCount" with will be "step" in reducer
-        setCount(1)
-      }}
-    />
-  )
-}
+    <Fragment>
+      <Button
+        text={`Count is ${count}`}
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      />
+      <LogData counter={count} />
+    </Fragment>
+  );
+};
 
-export const run = render(<Macro app={<App />} />)
+export const run = render(<Macro app={<App />} />);
